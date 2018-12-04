c535b5a8e7b7d9460577e325f1eb0d5c2514b39a
kobi <kobi.b@inner-active.com>
2018-12-04 22:13:28 +0200

    circleci

--- lib/fluttered.dart
+++ lib/fluttered.dart
@@ -1,6 +1,5 @@
 library fluttered;
 
-
-export 'src/stateful/scroll_builder.dart';
 export 'src/reuse/progress_builder.dart';
 export 'src/state/fetchState.dart';
+export 'src/stateful/scroll_builder.dart';
