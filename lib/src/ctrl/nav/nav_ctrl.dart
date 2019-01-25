bf336ef38b88acce431a3038f902537989b04769
kobi <kobi.b@inner-active.com>
2019-01-25 18:02:27 +0200

    fluttered

--- lib/src/ctrl/nav/nav_ctrl.dart
+++ lib/src/ctrl/nav/nav_ctrl.dart
@@ -1,26 +1,17 @@
-import 'package:action_tree_core/action_tree_core.dart';
 import 'package:fluttered/src/ctrl/nav/nav_state.dart';
+import 'package:mobx/mobx.dart';
 
-class NavCtrl extends Ctrl<NavState>{
+class NavCtrl {
   final NavState state;
 
-  Action<NavState> nav;
-
-  Iterable<Action<NavState>> get actions => [nav];
-
-  NavCtrl._(this.state) {
-    nav = Action(
-      ctrl: this,
-      name: "nav"
-    );
-  }
+  NavCtrl._(this.state);
 
   factory NavCtrl([NavState state]) {
     return NavCtrl._(state ?? NavState());
   }
 
   void navTo(String route, [Map<String, String> data = const {}, bool inherit = false]) {
-    nav.setState(() {
+    runInAction(() {
       state.route = route;
       state.changeData(data, inherit);
     });
