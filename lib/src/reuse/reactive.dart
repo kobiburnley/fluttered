a373cb4bb22f09acab687be6a2b2b90edc66cb00
kobi <kobi.b@inner-active.com>
2019-01-25 17:58:26 +0200

    use mobx

---
+++ lib/src/reuse/reactive.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/widgets.dart';
+import 'package:mobx/mobx.dart';
+
+// ignore:implementation_imports
+import 'package:mobx/src/core.dart' show ReactionImpl;
+
+typedef BuildObserved = Widget Function(BuildContext);
+
+class Reactive extends StatefulWidget {
+  const Reactive({@required this.builder, @required this.onInvalidate, Key key, this.context})
+      : assert(builder != null),
+        super(key: key);
+
+  final ReactiveContext context;
+  final BuildObserved builder;
+  final VoidCallback onInvalidate;
+
+  @visibleForTesting
+  Reaction createReaction(Function() onInvalidate) =>
+      ReactionImpl(context ?? mainContext, onInvalidate);
+
+  @override
+  State<Reactive> createState() => ReactiveState();
+}
+
+class ReactiveState extends State<Reactive> {
+  ReactionImpl _reaction;
+
+  @override
+  void initState() {
+    super.initState();
+    _reaction = widget.createReaction(widget.onInvalidate);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return widget.builder(context);
+  }
+
+  @override
+  void dispose() {
+    _reaction.dispose();
+    super.dispose();
+  }
+}
--- lib/src/reuse/router.dart
+++ lib/src/reuse/router.dart
@@ -1,26 +1,23 @@
-
-
-import 'package:action_tree_flutter/action_tree_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttered/src/ctrl/nav/nav.dart';
+import 'package:fluttered/src/reuse/reactive.dart';
 
 class Router extends StatelessWidget {
   final Map<String, WidgetBuilder> routes;
-  final ValueGetter<NavCtrl> getCtrl;
+  final NavCtrl ctrl;
   final Widget child;
 
-  Router({this.routes, this.child, this.getCtrl});
+  Router({this.routes, this.child, this.ctrl});
 
   Widget build(BuildContext context) {
-    return ObserverBuilder<NavState, NavCtrl>(
-      handleChange: (NavState state) {
-        WidgetBuilder builder = routes[state.route];
-        if(builder != null) {
+    return Reactive(
+      onInvalidate: () {
+        WidgetBuilder builder = routes[ctrl.state.route];
+        if (builder != null) {
           Navigator.of(context).push(MaterialPageRoute<Null>(builder: builder));
         }
       },
-      getCtrl: getCtrl,
-      builder: (_, __) => child,
+      builder: (_) => child,
     );
   }
 }
